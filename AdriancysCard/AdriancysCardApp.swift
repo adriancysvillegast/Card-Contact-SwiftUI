70298728af057607e7dbec71aee290b781055b9a
Adriancys Jesus Villegas Toro <81894293+adriancysvillegast@users.noreply.github.com>
2021-12-29 13:05:41 -0400

    Initial Commit

---
+++ AdriancysCard/AdriancysCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdriancysCardApp.swift
+//  AdriancysCard
+//
+//  Created by Adriancys Jesus Villegas Toro on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct AdriancysCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
